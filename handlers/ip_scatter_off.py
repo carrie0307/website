f20a9fdffd65e53a2fef6953924886c9aaa2f38b
carrie0307 <cst_study@163.com>
2017-09-09 09:50:09 +0800

    [update]ip-scatter搜索功能

--- handlers/ip_scatter_off.py
+++ handlers/ip_scatter_off.py
@@ -5,8 +5,12 @@ from methods.db_operation import ip_change
 class ipScatter_OffHandler(tornado.web.RequestHandler):
     def get(self):
         ip_change_res = ip_change('www-4s.cc')
-        print ip_change_res
+        # print ip_change_res
         self.write(ip_change_res)
 
     def post(self):
-        pass
+        domain = self.get_argument('domain')
+        print domain
+        ip_change_res = ip_change(domain)
+        # print ip_change_res
+        self.write(ip_change_res)
--- statics/js/ipscatter.js
+++ statics/js/ipscatter.js
@@ -38,7 +38,7 @@ option = {
             data:[],
             axisLabel:{
                 interval:'auto',
-                rotate:30,//旋转角度，为0表示水平
+                rotate:5,//旋转角度，为0表示水平
             }
         }
     ],
@@ -59,84 +59,100 @@ option = {
 
 // 使用刚指定的配置项和数据显示图表。
 
+//对get或post请求的内容进行处理，设置option相关参数
+function set_option(data1)
+{
+    real_data = data1["rows"];
+    var category_name = new Array(); //以每次访问时间作为x轴的category
+    var ini_ip_array = new Array(); // 原始的IP列表
+    var ip_array = new Array(); // 去重后的IP列表,作为Axis
 
-$.ajax({
+    var j=0;
+    var i = 0;
+
+    var real_series=[];
+    var xy=[]; //每次访问日期下实际对应的x.y轴数据
+
+    for(value in real_data)
+    {
+        for(i=0;i<real_data[value].length;i++)
+        {
+            xy.push([value,real_data[value][i]]);//实际的数据，x轴是日期，y周是ip对应的index（数字化）
+            //xy.push([real_data[value][i],value]);//实际的数据，x轴是日期，y周是ip对应的index（数字化）
+        }
+        real_series.push({name:value, type:'scatter',data:xy});
+        xy=[];
+
+        category_name[j]=value; //获取访问日期列表
+        ini_ip_array.push.apply(ini_ip_array, real_data[value]); //获取原始ip列表
+        j = j + 1;
+    }
+    category_name=category_name.sort();//按照时间进行排序
+
+    //对ip进行去重，得到ip_array
+    for(i=0;i<ini_ip_array.length;i++)
+    {
+        if (ip_array.indexOf(ini_ip_array[i]) == -1){
+            ip_array.push(ini_ip_array[i]);
+        }
+
+    }
+
+    option.xAxis[0].data=category_name;
+    option.yAxis[0].data=ip_array;
+    //option.xAxis[0].data=ip_array;
+    //option.yAxis[0].data=category_name;
+
+    option.series=real_series;
+    myChart.setOption(option);
+
+}
+
+
+
+$.ajax({ //用get方法请求加载默认域名的数据
     type:"get",
     url:"/ip_scatter_off",
     cache:false,
     success:function(data1){
-
-        alert("success");
-        real_data = data1["rows"];
-        var category_name = new Array(); //以每次访问时间作为x轴的category
-        var ini_ip_array = new Array(); // 原始的IP列表
-        var ip_array = new Array(); // 去重后的IP列表,作为Axis
-
-        var j=0;
-        var i = 0;
-
-        var real_series=[];
-        var xy=[]; //每次访问日期下实际对应的x.y轴数据
-
-        for(value in real_data)
-        {
-            for(i=0;i<real_data[value].length;i++)
-            {
-                xy.push([value,real_data[value][i]]);//实际的数据，x轴是日期，y周是ip对应的index（数字化）
-                //xy.push([real_data[value][i],value]);//实际的数据，x轴是日期，y周是ip对应的index（数字化）
-            }
-            real_series.push({name:value, type:'scatter',data:xy});
-            xy=[];
-
-    		category_name[j]=value; //获取访问日期列表
-            ini_ip_array.push.apply(ini_ip_array, real_data[value]); //获取原始ip列表
-            j = j + 1;
-	    }
-        category_name=category_name.sort();//按照时间进行排序
-
-
-        //对ip进行去重，得到ip_array
-        for(i=0;i<ini_ip_array.length;i++)
-        {
-            if (ip_array.indexOf(ini_ip_array[i]) == -1){
-                ip_array.push(ini_ip_array[i]);
-            }
-
-        }
-
-
-        /*
-        for(value in real_data)
-        {
-
-            for(i=0;i<real_data[value].length;i++)
-            {
-                xy.push([value,real_data[value][i]]);//实际的数据，x轴是日期，y周是ip对应的index（数字化）
-                //xy.push([real_data[value][i],value]);//实际的数据，x轴是日期，y周是ip对应的index（数字化）
-            }
-            real_series.push({name:value, type:'scatter',data:xy});
-            xy=[];
-        }
-        */
-        option.xAxis[0].data=category_name;
-        option.yAxis[0].data=ip_array;
-
-        //option.xAxis[0].data=ip_array;
-        //option.yAxis[0].data=category_name;
-
-        option.series=real_series;
-        alert(option.series[0]['data'][3][1]);
-        myChart.setOption(option);
-        //option.yAxis[0].axisLabel['formatter']=ip_array[value];
-        //myChart.setOption(option);
-        //alert(option.series[0]['name'][3][1]);
-
-
+        set_option(data1); //调用函数，对请求所的data进行处理，并设置option
     },
     error:function(){
         alert("error!");
     },
+});
+
+
+$(document).ready(function(){
+  //搜索框按钮事件处理
+  $("#search-btn").click(function(e){
+      var wd=$('#search-blank').val();
+      alert(wd);
+
+    /*
+    这里要注意的是，起初没有写e.preventDefault();，浏览器debug报错uncaught exception: out of memory in Ajax Process，
+    根据 https://stackoverflow.com/questions/29291952/uncaught-exception-out-of-memory-in-ajax-process 的结果，添加了
+    e.preventDefault(); ， 同时注意在functin的括号中加上‘e’
+    */
+
+      e.preventDefault();
+      $.ajax({ //用get方法请求加载默认域名的数据
+          type:"post",
+          url:"/ip_scatter_off",
+          cache:false,
+          data:{domain:wd},
+          success:function(data1){
+              alert("success");
+              set_option(data1); //调用函数，对请求所的data进行处理，并设置option
+          },
+          error:function(){
+              alert("error!");
+          },
+      });
+
+  });
 
 });
+
 
 myChart.setOption(option);// 初次加载图表(无数据)
